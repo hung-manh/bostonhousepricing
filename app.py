4d8b8ca515457f5c9c6e3becc9b263adaed134ae
hung-manh <hmh290204@gmail.com>
2024-07-01 14:27:41 +0700

    App ready to use

--- app.py
+++ app.py
@@ -26,5 +26,15 @@ def predict_api():
     output = prediction[0]
     return jsonify(output)
 
+@app.route('/predict', methods=['POST'])
+def predict12():
+    data = [float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1, -1)) 
+    prediction = regmodel.predict(final_input)[0]
+    # Can return the prediction in any format
+    return render_template('home.html', prediction_text2hello='The House preice prediction is: {}'.format(prediction),
+                            prediction_text2='The House preice prediction is: {}'.format(prediction))
+
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
